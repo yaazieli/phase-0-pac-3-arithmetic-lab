d80f03fe699487e6a9d3368696d0a7f1933ee897
Yaazieli <yaazieli@gmail.com>
2021-10-02 22:38:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,53 @@
+let a = Math.floor(Math.random() * 1000)
+let b = Math.floor(Math.random() * 1000)
+function add(a, b) {
+return a+b;
+}
+add(a, b);
+
+let c = Math.floor(Math.random() * 1000)
+let d = Math.floor(Math.random() * 1000)
+function subtract(c, d){
+return c-d;
+}
+subtract(c, d);
+
+let e = Math.floor(Math.random() * 1000)
+let f = Math.floor(Math.random() * 1000)
+function multiply(e, f){
+return e*f;
+}
+multiply(e, f);
+
+let g = Math.floor(Math.random() * 1000);
+let h = Math.floor(Math.random() * 1000);
+function divide(g, h){
+return g/h;
+}
+divide(g, h);
+
+let i = Math.floor(Math.random() * 1000);
+function decrement(i) {
+return i-1;
+}
+decrement(n);
+
+let j = Math.floor(Math.random() * 1000);
+function increment(j) {
+return j+1;
+}
+increment(n);
+
+
+
+let o = Math.floor(Math.random() * 1000);
+function makeInt(o) {
+return parseInt(o, 10);
+}
+makeInt(o);
+
+let p = Math.floor(Math.random() * 1000);
+function preserveDecimal(l) {
+return parseFloat(l, 10);
+}
+preserveDecimal(l);
